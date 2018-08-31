66e136a56a09e88798ca15aadf5936fe10422c35
Miario <mario.indyushkin@gmail.com>
2018-08-30 21:29:31 -0700

    updating about section

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown, faDownload, faDesktop, faClock, faEdit, faBook, faCode } from '@fortawesome/free-solid-svg-icons';
+import { faAngleDown, faDownload, faDesktop, faClock, faEdit, faBook, faCode, faPencilAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
 import resumeImage from './resume.png';
 import resumePDF from './resume.pdf';
 import toggleResume from './toggle';
@@ -56,16 +56,19 @@ class About extends Component {
                             </p>
                             <div className="flex-service-row">
                                 <div className="services">
-                                    <div className="square" data-aos='flip-right' data-aos-delay="100"><FontAwesomeIcon icon={faCode} size="2x" color="white" className="icon-center" /></div>
-                                    <p className="" data-aos='fade' data-aos-delay="100">Bringing ideas to life in the browser Bringing ideas to life in the browserv</p>
+                                    <div className="square" data-aos='flip-right' data-aos-delay="100"><FontAwesomeIcon icon={faPencilAlt} size="2x" color="white" className="icon-center" /></div>
+                                    <h3 className="tite">Design</h3>
+                                    <p className="" data-aos='fade' data-aos-delay="100">Adobe Suite, Gimp, Pen & Paper</p>
                                 </div>
                                 <div className="services">
                                     <div className="square" data-aos='flip-right' data-aos-delay="150"><FontAwesomeIcon icon={faCode} size="2x" color="white" className="icon-center"/></div>
-                                    <p className="" data-aos='fade' data-aos-delay="150">Bringing ideas to life in the browser Bringing ideas to life in the browserv</p>
+                                    <h3 className="tite">Front End Developer</h3>
+                                    <p className="" data-aos='fade' data-aos-delay="150">HTML, CSS, JavaScript, React, Bootstrap, CMS</p>
                                 </div>
                                 <div className="services">
-                                    <div className="square" data-aos='flip-right' data-aos-delay="200"><FontAwesomeIcon icon={faCode} size="2x" color="white" className="icon-center" /></div>
-                                    <p className="" data-aos='fade' data-aos-delay="200">Bringing ideas to life in the browser Bringing ideas to life in the browserv</p>
+                                    <div className="square" data-aos='flip-right' data-aos-delay="200"><FontAwesomeIcon icon={faWrench} size="2x" color="white" className="icon-center" /></div>
+                                    <h3 className="tite">Tools</h3>
+                                    <p className="" data-aos='fade' data-aos-delay="200">Git, VS Code</p>
                                 </div>
                             </div>
                         </div>
@@ -76,7 +79,7 @@ class About extends Component {
                             type="button" 
                             className="btn-resume btn-color"
                             >
-                            <FontAwesomeIcon icon={faAngleDown} size="lg" color="white" /> View Resume
+                            <FontAwesomeIcon icon={faAngleDown} size="lg" color="white" /> View Full Resume
                         </button>
                         <div id="resume" className="resume-border">
                             <img src={resumeImage} alt="Resume" />
