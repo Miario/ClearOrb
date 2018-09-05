54b13da77fabf2fbd425e95843d12d017706f36e
Miario <mario.indyushkin@gmail.com>
2018-09-05 13:49:20 -0700

    updating styles

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,9 +13,9 @@ class Footer extends Component {
                     <p>&copy; Copyright ClearOrb 2018</p>
                     <a href="#home" className="arrow-up"><FontAwesomeIcon icon={faAngleDoubleUp} /></a>
                     <div className="social"> 
-                        <a href="mailto:clearorbi@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="2x"/></a>
-                        <a href="https://www.linkedin.com/in/mario-indyushkin-25092a153" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" /></a>
-                        <a href="https://github.com/Miario" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" /></a>
+                        <a href="mailto:clearorbi@gmail.com"><FontAwesomeIcon icon={faEnvelope} size="2x" className="footer-icon"/></a>
+                        <a href="https://www.linkedin.com/in/mario-indyushkin-25092a153" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedin} size="2x" className="footer-icon"/></a>
+                        <a href="https://github.com/Miario" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} size="2x" className="footer-icon"/></a>
                     </div>
                 </div>
             </div>
