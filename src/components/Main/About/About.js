46f86af676ef338c17cb310a962e25dd2b720400
Miario <mario.indyushkin@gmail.com>
2018-09-10 10:23:12 -0700

    updating component

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -3,10 +3,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAngleDown, faDownload, faDesktop, faClock, faEdit, faBook, faCode, faPencilAlt, faWrench } from '@fortawesome/free-solid-svg-icons';
 import resumeImage from './resume.png';
 import resumePDF from './resume.pdf';
-import toggleResume from './toggle';
 import './About.css';
 
 class About extends Component {
+    constructor() {
+        super();
+        this.state = {
+            active: false
+        };
+        this.toggleResume = this.toggleResume.bind(this);
+    }
+
+    toggleResume() {
+        const currentState = this.state.active;
+        this.setState({active: !currentState});
+    }
+
     render() {
         return (
             <section id="about">
@@ -72,13 +84,13 @@ class About extends Component {
                     </div>
                     <div>
                         <button 
-                            onClick={toggleResume} 
+                            onClick={this.toggleResume}
                             type="button" 
                             className="btn-resume btn-color"
                             >
                             <FontAwesomeIcon icon={faAngleDown} size="lg" color="white" /> View Full Resume
                         </button>
-                        <div id="resume" className="resume-border">
+                        <div id="resume" className={this.state.active ? 'visible resume-border' : 'hidden'}>
                             <img src={resumeImage} alt="Resume" />
                             <a href={resumePDF} className="btn-resume btn-download btn-color" download><FontAwesomeIcon icon={faDownload} size="sm" color="white" /> Download</a>
                         </div>
